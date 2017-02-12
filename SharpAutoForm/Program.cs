75bb421512850137fbf312f20b929b3edb77df6c
HarryKural <harrykural333@gmail.com>
2017-02-12 18:09:07 -0500

    Added header comment in some files

--- SharpAutoForm/Program.cs
+++ SharpAutoForm/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/// <summary>
+/// App name:           Sharp Auto Form
+/// Author's name:      Harshit Sharma
+/// Student Number#:    200333254
+/// App Creation Date:  Feb 04, 2017
+/// Last Modified Date: Feb 12, 2017
+/// App description:    This program calculates the
+///                     amount due on a New or Used Vehicle based on
+///                     accessories and options selected and a trade-in value (if any).
+/// Rapid Application Development - Assignment 2
+/// </summary>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
